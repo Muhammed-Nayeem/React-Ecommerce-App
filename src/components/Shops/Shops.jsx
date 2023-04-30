a140c5f92c298f4cc90c82934a46d7b4bc79941f
Muhammed Nayeem <mohammednayeem808@gmail.com>
2023-04-30 13:09:44 +0600

    Modified to Create dynamic product component and added some css style

--- src/components/Shops/Shops.jsx
+++ src/components/Shops/Shops.jsx
@@ -1,21 +1,24 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import "./Shops.css";
+import Product from "../Product/Product";
 
 const Shops = () => {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
     fetch("/products.json")
-      .then(res => res.json())
-      .then(data => setProducts(data))
-  }, [])
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
 
   return (
     <section className="shops-area">
       <div className="container">
         <div className="shops-wrapper">
           <div className="products-container">
-            <h3>Products Coming Here: {products.length}</h3>
+            {products.map((product) => (
+              <Product product={product} key={product.id}></Product>
+            ))}
           </div>
           <div className="cart-container">
             <h3>Order Summary</h3>
