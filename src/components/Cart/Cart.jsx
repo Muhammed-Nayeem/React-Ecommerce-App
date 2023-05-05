ccbb5c9ee3d077ab2c5e452dff6923e0b4528dc7
Muhammed Nayeem <mohammednayeem808@gmail.com>
2023-05-05 15:21:23 +0600

    Added Local Storage Mechanism to Manage Data

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -8,12 +8,20 @@ const Cart = (props) => {
   //total price, shipping charges, taxes calculation:
   let totalPrice = 0;
   let totalShippingCharges = 0;
-  let totalTax = 0;
+  let totalQuantity = 0;
   for (const product of cart) {
-    totalPrice = totalPrice + product.price;
-    totalShippingCharges = totalShippingCharges + product.shipping;
-    totalTax = totalTax + (product.price * 7) / 100;
+    // if (product.id === 0) {
+    //   product.id = 1;
+    // }
+    // product.quantity = product.quantity || 1;
+
+    totalPrice = totalPrice + product.price * product.quantity;
+    totalShippingCharges = totalShippingCharges + product.shipping * product.quantity;
+    totalQuantity = totalQuantity + product.quantity;
   }
+
+  //total tax calculation:
+  let totalTax = (totalPrice * 7) / 100;
 
   //Grand Total Price:
   const grandTotal = totalPrice + totalShippingCharges + totalTax;
@@ -21,7 +29,7 @@ const Cart = (props) => {
   return (
     <div className="cart">
       <h4>Order Summary</h4>
-      <p>Selected Items: {cart.length}</p>
+      <p>Selected Items: {totalQuantity}</p>
       <p>Total Price: ${totalPrice}</p>
       <p>Total Shipping Charges: ${totalShippingCharges}</p>
       <p>Tax: ${totalTax.toFixed(2)}</p>
--- src/components/Shops/Shops.jsx
+++ src/components/Shops/Shops.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import "./Shops.css";
 import Product from "../Product/Product";
 import Cart from "../Cart/Cart";
+import { addToDb, getShoppingCart } from "../../utilities/fakedb";
 
 const Shops = () => {
   const [products, setProducts] = useState([]);
@@ -14,10 +15,48 @@ const Shops = () => {
       .then((data) => setProducts(data));
   }, []);
 
+  //get the data form local storage:
+  useEffect(() => {
+    //get stored shopping-cart:
+    const storedCart = getShoppingCart();
+    //new saved-cart:
+    const savedCart = [];
+    //step 1 get the product id from stored cart:
+    for (const id in storedCart) {
+      //step 2 find the product data from product state by using id:
+      const addedProduct = products.find((product) => product.id === id);
+      if (addedProduct) {
+        //step 3 set the quantity value:
+        const quantity = storedCart[id];
+        addedProduct.quantity = quantity;
+        //step 4 added the product with quantity:
+        savedCart.push(addedProduct);
+      }
+    }
+    //step 5 update the setCart:
+    setCart(savedCart);
+  }, [products]);
+
   //add to cart button handler:
   const addToCartHandler = (product) => {
-    let newCart = [...cart, product];
+    // let newCart = [...cart, product];
+
+    //For quantity : cart is modified
+    let newCart = [];
+
+    //if the product is not exist in cart, then set the quantity = 1:
+    //if exist then update the quantity:
+    const exists = cart.find((pd) => pd.id === product.id);
+    if (!exists) {
+      product.quantity = 1;
+      newCart = [...cart, product];
+    } else {
+      exists.quantity = exists.quantity + 1;
+      const remaining = cart.filter((pd) => pd.id !== product.id);
+      newCart = [...remaining, exists];
+    }
     setCart(newCart);
+    addToDb(product.id);
   };
 
   return (
